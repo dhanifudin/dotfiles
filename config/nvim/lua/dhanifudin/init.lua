afbcd276d71df4e331044fd4a4edbac1f59b4b68
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2022-09-12 19:19:19 +0900

    update nvim config

--- config/nvim/lua/dhanifudin/init.lua
+++ config/nvim/lua/dhanifudin/init.lua
@@ -1,15 +1,15 @@
 local is_vscode = vim.api.nvim_eval('exists("g:vscode")') == 1
 
 if not is_vscode then
-  require('dhanifudin.lsp')
-  require('dhanifudin.lspsaga')
-  require('dhanifudin.treesitter')
-  require('dhanifudin.easymotion')
-  require('dhanifudin.mapping')
-  require('dhanifudin.plugins')
-  require('dhanifudin.config')
-  require('dhanifudin.lualine')
-  require('dhanifudin.blankline')
-  require('dhanifudin.telescope')
-  require('dhanifudin.formatter')
+   require('dhanifudin.plugins')
+   require('dhanifudin.lsp')
+   require('dhanifudin.lspsaga')
+   require('dhanifudin.treesitter')
+   require('dhanifudin.easymotion')
+   require('dhanifudin.mapping')
+   require('dhanifudin.config')
+   require('dhanifudin.lualine')
+   require('dhanifudin.blankline')
+   require('dhanifudin.telescope')
+   require('dhanifudin.formatter')
 end
