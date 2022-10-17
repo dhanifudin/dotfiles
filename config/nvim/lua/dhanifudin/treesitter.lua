bd4e7cf2360b03c3c0f2ec0514d2f655a117a1ac
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2022-10-17 17:57:45 +0900

    use treesitter for foldmethod

--- config/nvim/lua/dhanifudin/treesitter.lua
+++ config/nvim/lua/dhanifudin/treesitter.lua
@@ -1,18 +1,24 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
-  highlight = {
-    enable = true,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.outer",
-        ["ac"] = "@function.outer",
-        ["ic"] = "@function.outer",
-      }
-    }
-  }
-}
+local vim = vim
+local opt = vim.opt
+
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "all",
+	highlight = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.outer",
+				["ac"] = "@function.outer",
+				["ic"] = "@function.outer",
+			},
+		},
+	},
+})
