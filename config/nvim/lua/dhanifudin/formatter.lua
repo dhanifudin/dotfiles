18aa77116475b4c79879056d6844bb0dba014ed2
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2022-11-15 19:03:56 +0900

    cleanup formatter

--- config/nvim/lua/dhanifudin/formatter.lua
+++ config/nvim/lua/dhanifudin/formatter.lua
@@ -1,77 +1,14 @@
--- Utilities for creating configurations
-local util = require("formatter.util")
-
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup({
-	-- Enable or disable logging
 	logging = true,
-	-- Set the log level
 	log_level = vim.log.levels.WARN,
-	-- All formatter configurations are opt-in
 	filetype = {
-		-- Formatter configurations for filetype "lua" go here
-		-- and will be executed in order
-		lua = {
-			-- "formatter.filetypes.lua" defines default configurations for the
-			-- "lua" filetype
-			require("formatter.filetypes.lua").stylua,
-
-			-- You can also define your own configuration
-			function()
-				-- Supports conditional formatting
-				if util.get_current_buffer_file_name() == "special.lua" then
-					return nil
-				end
-
-				-- Full specification of configurations is down below and in Vim help
-				-- files
-				return {
-					exe = "stylua",
-					args = {
-						"--search-parent-directories",
-						"--stdin-filepath",
-						util.escape_path(util.get_current_buffer_file_path()),
-						"--",
-						"-",
-					},
-					stdin = true,
-				}
-			end,
-		},
-
-		go = {
-			function()
-				return {
-					exe = "gofmt",
-					stdin = true,
-				}
-			end,
-		},
-
-		haskell = {
-			function()
-				return {
-					exe = "stylish-haskell",
-					stdin = true,
-				}
-			end,
-		},
-
-		python = {
-			function()
-				return {
-					exe = "black",
-					args = { "-q", "-" },
-					stdin = true,
-				}
-			end,
-		},
-
-		-- Use the special "*" filetype for defining formatter configurations on
-		-- any filetype
+		lua = { require("formatter.filetypes.lua").stylua },
+		go = { require("formatter.filetypes.go").gofmt },
+		haskell = { require("formatter.filetypes.haskell").stylish_haskell },
+		python = { require("formatter.filetypes.python").black },
+		yaml = { require("formatter.filetypes.yaml").yamlfmt },
 		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any
-			-- filetype
 			require("formatter.filetypes.any").remove_trailing_whitespace,
 		},
 	},
