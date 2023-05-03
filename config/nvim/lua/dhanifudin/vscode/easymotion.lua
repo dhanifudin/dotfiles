536db3244bfe8996e4e035871329cd2297612a22
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2023-05-03 15:56:14 +0900

    replace easymotion with hop

--- config/nvim/lua/dhanifudin/vscode/easymotion.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"asvetliakov/vim-easymotion",
-	config = function()
-		vim.g.EasyMotion_do_mapping = 0
-		vim.g.EasyMotion_smartcase = 1
-		vim.keymap.set({ "n", "o", "x" }, "s", "<Plug>(easymotion-s)")
-	end,
-}
