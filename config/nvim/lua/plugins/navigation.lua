bd9d3a22d9db0ff9881f1d7ea386419631b3395f
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2023-08-17 05:11:59 +0900

    add jump labels

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -1,4 +1,14 @@
 return {
   { "christoomey/vim-tmux-navigator" },
   { "tpope/vim-vinegar" },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
+  },
 }
