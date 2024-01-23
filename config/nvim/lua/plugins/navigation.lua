d20c187d67c08500c7ebad4e91291a53a22ea932
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2024-01-23 18:54:53 +0700

    update tmux plugin

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -1,5 +1,42 @@
 return {
-  { "christoomey/vim-tmux-navigator" },
+  {
+    "aserowy/tmux.nvim",
+    keys = {
+      {
+        "<C-h>",
+        function()
+          require("tmux").move_left()
+        end,
+        desc = "tmux move left",
+      },
+      {
+        "<C-j>",
+        function()
+          require("tmux").move_bottom()
+        end,
+        desc = "tmux move bottom",
+      },
+      {
+        "<C-k>",
+        function()
+          require("tmux").move_top()
+        end,
+        desc = "tmux move top",
+      },
+      {
+        "<C-l>",
+        function()
+          require("tmux").move_right()
+        end,
+        desc = "tmux move right",
+      },
+    },
+    opts = {
+      navigation = {
+        enable_default_keybindings = false,
+      },
+    },
+  },
   { "tpope/vim-vinegar" },
   {
     "folke/flash.nvim",
