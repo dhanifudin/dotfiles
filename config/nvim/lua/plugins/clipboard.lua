b1265f54d85566794da94029025464260861e8d5
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2024-02-05 08:39:31 +0700

    update clipboard config

--- config/nvim/lua/plugins/clipboard.lua
+++ config/nvim/lua/plugins/clipboard.lua
@@ -10,28 +10,4 @@ return {
       end
     end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = function()
-      vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-      vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-      vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
-      vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
-      vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
-      vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
-      local mapping = require("yanky.telescope.mapping")
-      local mappings = mapping.get_defaults()
-      mappings.i["<c-p>"] = nil
-      return {
-        highlight = { timer = 200 },
-        ring = { storage = jit.os:find("Windows") and "shada" or "sqlite" },
-        picker = {
-          telescope = {
-            use_default_mappings = false,
-            mappings = mappings,
-          },
-        },
-      }
-    end,
-  },
 }
