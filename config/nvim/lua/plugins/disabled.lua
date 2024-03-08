c252b0d4063035124b5e232f75a28c9f80d7f682
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2024-03-08 14:30:56 +0700

    update colorscheme

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,4 @@
 return {
-  { "catppuccin/nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
 }
+
--- config/nvim/lua/plugins/eyecandy.lua
+++ config/nvim/lua/plugins/eyecandy.lua
@@ -1,9 +1,8 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
   {
