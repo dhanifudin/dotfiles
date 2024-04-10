3492eba318084ade70aaa7cda20550cae4fc5e62
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2024-04-11 03:18:48 +0700

    update the laravel config

--- config/nvim/lua/plugins/laravel.lua
+++ config/nvim/lua/plugins/laravel.lua
@@ -1,5 +1,11 @@
 return {
   {
-    "jwalton512/vim-blade",
+    "noahfrederick/vim-laravel",
+    dependencies = {
+      "tpope/vim-dispatch",
+      "tpope/vim-projectionist",
+      "noahfrederick/vim-composer",
+      "jwalton512/vim-blade",
+    },
   },
 }
