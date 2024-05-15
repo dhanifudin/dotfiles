d09b42881f11ba660250def08762661614c66d64
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2024-05-15 15:29:41 +0000

    add oil.nvim

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -32,4 +32,24 @@ return {
       },
     },
   },
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function()
+      local oil = require("oil")
+      oil.setup {
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      vim.keymap.set("n", "<space>-", oil.toggle_float)
+    end
+  }
 }
