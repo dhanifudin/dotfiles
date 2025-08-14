a356c9f38c02eebcd652d3850bcc86ef54ee9a35
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2025-08-14 08:05:04 +0700

    update home-manager

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -1,27 +1,60 @@
 { config, pkgs, ...}:
 
 {
-  home.username = "dhs";
-  home.homeDirectory = "/home/dhs";
+  nixpkgs.config.allowUnfree = true;
+
+  home = {
+    username = "dhs";
+    homeDirectory = "/home/dhs";
+    stateVersion = "24.11";
+    shellAliases = {
+        google-chrome = "google-chrome-stable";
+    };
+  };
 
   home.packages = with pkgs; [
+    android-tools
+    ansible
     asdf-vm
     fasd
     fd
+    flutter
     fzf
     git
+    google-chrome
     home-manager
     htop
     lazygit
     lua51Packages.luarocks-nix
+    mdbook
     neofetch
     neovim
+    pandoc
     ripgrep
     rustup
+    sdkmanager
     silver-searcher
     tmux
     tree
+    unzip
+    virtualgl
+    zimfw
+    zip
   ];
 
-  home.stateVersion = "24.11";
+  programs.zsh = {
+    enable = true;
+    initContent = ''
+      bindkey -e
+
+      ZIM_HOME=$HOME/.zim
+      if [ -e $HOME/.nix-profile/zimfw.zsh ]; then . $HOME/.nix-profile/zimfw.zsh init; fi
+
+      export PATH="$HOME/.config/composer/vendor/bin:$HOME/bin:$HOME/.asdf/shims:$PATH"
+      . $HOME/.asdf/plugins/java/set-java-home.zsh
+      eval "$(starship init zsh)"
+      if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi
+      export ADB_SERVER_SOCKET=tcp:$(ip route show | grep -i default | awk '{ print $3}'):5037
+    '';
+  };
 }
